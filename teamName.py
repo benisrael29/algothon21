2a47f9bf6ac18408f6cd8b8ced72878497253ba7
Benjamin Israel <62453991+benisrael29@users.noreply.github.com>
2021-07-26 12:50:24 +1000

    Update teamName.py
    Team Strength in Numbers Algo

--- teamName.py
+++ teamName.py
@@ -1,20 +1,60 @@
-#!/usr/bin/env python
-
-# RENAME THIS FILE WITH YOUR TEAM NAME.
-
+import pandas as pd
 import numpy as np
 
-nInst=100
+nInst = 100
 currentPos = np.zeros(nInst)
 
-# Dummy algorithm to demonstrate function format.
-def getMyPosition (prcSoFar):
+def getMyPosition(prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    rpos = np.array([int(x) for x in 1000 * np.random.randn(nins)])
-    currentPos += rpos
-    # The algorithm must return a vector of integers, indicating the position of each stock.
-    # Position = number of shares, and can be positve or negative depending on long/short position.
-    return currentPos
+    prcSoFar = pd.DataFrame(prcSoFar)
+    short_ema = prcSoFar.ewm(span=5, adjust=False, axis=1).mean()
+    # 5 day exponential moving average
+    posSize = 2500
+    #base position size
+    buy = posSize
+    sell = -posSize
+    buy_big = posSize*4
+    sell_big = -posSize*4
+    rpos = np.zeros(nInst)
+    trade_count = np.zeros(nInst)
+    days = prcSoFar.shape[1]
+    curr_pos = np.zeros(nInst)
+    r_pos = np.zeros(nInst)
+    stock_pnl = np.zeros(nInst)
+    ev = np.zeros(nInst)
+    
+    #loop to filter for +ev instruments
+    for n in range(days):
+        r_pos = np.zeros(nInst)
+        for m in range(100):
+            if prcSoFar.loc[m, n] < (short_ema.loc[m, n] * 0.991):
+                r_pos[m] += round(buy_big/prcSoFar.loc[m, n])
+            if prcSoFar.loc[m, n] > (short_ema.loc[m, n] * 1.009):
+                r_pos[m] += round(sell_big/prcSoFar.loc[m, n])
+            stock_pnl[m] += curr_pos[m] * prcSoFar.loc[m, n] - curr_pos[m] * prcSoFar.iloc[m, n-1]
+                
+        if n > 1:
+            for j in range(0,100):
+                if curr_pos[j]*prcSoFar.loc[j,n] != curr_pos[j]*prcSoFar.loc[j, n-1]:
+                    trade_count[j] += 1
+            
+        curr_pos = r_pos
 
+    for i in range(100):
+        if trade_count[i] > 0:
+            ev[i] = stock_pnl[i] / trade_count[i]
+        if prcSoFar.loc[i, nt-1] < (short_ema.loc[i, nt-1] * 0.991):
+            if ev[i] == 0:
+                rpos[i] += round(buy/prcSoFar.loc[i, nt-1])
+            if ev[i] > 0:
+                rpos[i] += round(buy_big/prcSoFar.loc[i, nt-1])
+        if prcSoFar.loc[i, nt-1] > (short_ema.loc[i, nt-1] * 1.009):
+            if ev[i] == 0:
+                rpos[i] += round(sell/prcSoFar.loc[i, nt-1])
+            if ev[i] > 0:
+                rpos[i] += round(sell_big/prcSoFar.loc[i, nt-1])
+                
+    currentPos = rpos
+    return currentPos
     
